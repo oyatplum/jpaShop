9cc74f7ca0945fe457b86c9d1ca991a03dd74f8b
이예지 <oyatpeach@naver.com>
2024-07-11 15:04:50 +0900

    Feat : domain 생성

---
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,21 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+public class Delivery {
+    @Id @GeneratedValue
+    private Long id;
+
+    @OneToOne(mappedBy = "delivery", fetch = FetchType.LAZY)
+    private Order order;
+
+    @Embedded
+    private Address address;
+
+    @Enumerated(EnumType.STRING)
+    private DeliveryStatus status; // READY, COMP
+}
